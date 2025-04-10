d672b33ed4fa25a2a76f5eeb00a28b9222556732
ondal <hiondal@gmail.com>
2025-04-10 23:11:00 +0900

    add manifest

--- public/runtime-env.js
+++ public/runtime-env.js
@@ -1,5 +1,5 @@
 window.__runtime_config__ = {
-    MEMBER_URL: 'http://localhost:8081/api/auth',
-    MYSUB_URL: 'http://localhost:8082/api/mysub',
-    RECOMMEND_URL: 'http://localhost:8083/api/recommend'
+    MEMBER_URL: 'http://20.249.205.95/api/auth',
+    MYSUB_URL: 'http://20.249.205.95/api/mysub',
+    RECOMMEND_URL: 'http://20.249.205.95/api/recommend'
 };
